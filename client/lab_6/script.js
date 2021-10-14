768108a604aa85c562cb9dd44f3688392265b2bc
jremeto <jordanremeto@Jordans-MacBook-Air-2.local>
2021-10-14 17:14:04 -0400

    Lab 6 Finished

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,40 +1,39 @@
-const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json'
+async function windowActions() {
+  const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
 
-const cities = [];
+  // const cities = [];
 
-fetch(endpoint, function(data) {
-  console.log(data);
-});
+  const request = await fetch(endpoint)
 
-fetch(endpoint)
-  .then((blob) => blob.json())
-  .then((data) => cities.push(...data));
+  const cities = await request.json()
 
-function findMatches(wordToMatch, cities) {
-  return cities.filter(place => {
-    const regex = new RegExp(wordToMatch, 'gi');
-    return place.city.match(regex) || place.state.match(regex);
-  });
+  function findMatches(wordToMatch, cities) {
+    return cities.filter(place => {
+      const regex = new RegExp(wordToMatch, 'gi');
+      return place.city.match(regex) || place.state.match(regex);
+    });
+  }
+
+  function displayMatches(event) {
+    const matchArray = findMatches(event.target.value, cities);
+    const html = matchArray.map((place) => {
+      const regex = new RegExp(this.value, 'gi');
+      const cityName = place.city.replace(regex, `<span class = 'hl'>${this.value}</span>`);
+      const stateName = place.state.replace(regex, `<span class = 'hl'>${this.value}</span>`);
+      return `
+        <li> 
+          <span class = 'name'>${place.city}, ${place.state}</span>
+          <span class = 'restaurant'>${place.name}</span>
+        <li>
+      `;
+    }).join('');
+    suggestions.innerHTML = html;
+  }
+
+  const searchInput = document.querySelector('.search');
+  const suggestions = document.querySelector('.suggestions');
+
+  searchInput.addEventListener('change', displayMatches);
+  searchInput.addEventListener('keyup', (evt) => { displayMatches(evt) });
 }
-
-function displayMatches() {
-  const matchArray = findMatches(this.value, cities);
-  const html = matchArray.map((place) => {
-    const regex = new RegExp(this.value, 'gi');
-    const cityName = place.city.replace(regex, `<span class = 'hl'>${this.value}</span>`);
-    const stateName = place.state.replace(regex, `<span class = 'hl'>${this.value}</span>`);
-    return `
-      <li> 
-        <span class = 'name'>${cityName}, ${stateName}</span>
-        <span class = 'population'>${place.population}</span>
-      <li>
-    `;
-  }).join('');
-  suggestions.innerHTML = html;
-}
-
-const searchInput = document.querySelector('.search');
-const suggestions = document.querySelector('.suggestions');
-
-searchInput.addEventListener('change', displayMatches);
-searchInput.addEventListener('keyup', displayMatches);
+window.onload = windowActions;
