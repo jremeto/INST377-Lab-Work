9ed4d64f1961a1dc0a8c2dc802bce32f864898d3
jremeto <jordanremeto@Jordans-MacBook-Air-2.local>
2021-10-19 21:11:27 -0400

    Lab 7 Start

---
+++ client/lab_7/script.js
@@ -0,0 +1,50 @@
+async function windowActions() {
+  const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
+
+  const accessToken = '';
+
+  const request = await fetch(endpoint);
+
+  const cities = await request.json();
+
+  const mymap = L.map('mapid').setView([51.505, -0.09], 13);
+  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox/streets-v11',
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken: 'your.mapbox.access.token'
+    }).addTo(mymap);
+  }
+
+  function findMatches(wordToMatch, cities) {
+    return cities.filter(place => {
+      const regex = new RegExp(wordToMatch, 'gi');
+      return place.city.match(regex) || place.state.match(regex);
+    });
+  }
+
+  function displayMatches(event) {
+    const matchArray = findMatches(event.target.value, cities);
+    const html = matchArray.map((place) => {
+      const regex = new RegExp(this.value, 'gi');
+      const cityName = place.city.replace(regex, `<span class = 'hl'>${this.value}</span>`);
+      const stateName = place.state.replace(regex, `<span class = 'hl'>${this.value}</span>`);
+      return `
+        <li> 
+          <span class = 'name'>${place.city}, ${place.state}</span>
+          <span class = 'restaurant'>${place.name}</span>
+        <li>
+      `;
+    }).join('');
+    suggestions.innerHTML = html;
+  }
+
+  const searchInput = document.querySelector('.search');
+  const suggestions = document.querySelector('.suggestions');
+
+  searchInput.addEventListener('change', displayMatches);
+  searchInput.addEventListener('keyup', (evt) => { displayMatches(evt) });
+}
+window.onload = windowActions;
